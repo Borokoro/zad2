3fb23c6e1b7718d4f9160bb2de2788bd783954d5
Borokoro <marek0cichon@gmail.com>
2023-01-26 21:42:13 +0100

    Initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      MultiFib
+      <br></br>
+      Marek Cichoń
+      <br></br>
+      IMST 2.1/2
+      <br></br>
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
